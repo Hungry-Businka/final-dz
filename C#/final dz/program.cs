8165d45f5475833964beb1fa540e52f82c8f80ef
«Denis» <sup.zx@yandex.ru>
2022-10-02 21:09:38 +0300

    Вывод массива

---
+++ C#/final dz/program.cs
@@ -0,0 +1,12 @@
+﻿string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
+string[] array2 = new string[array1.Length];
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+PrintArray(array1);
